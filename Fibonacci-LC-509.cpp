05e31dbc67f5cb90172784cd0e3d4ab3305c0fc4
Adit Garg <141175997+adit-garg@users.noreply.github.com>
2024-08-11 02:11:35 +0530

    Leetcode Problem 509

---
+++ Fibonacci-LC-509.cpp
@@ -0,0 +1,10 @@
+// LeetCode Problem #509 Fibonacci Number (11 ms)
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n ==0) return 0;
+        if (n == 1) return 1;
+        else return fib(n-1) + fib(n-2);
+    }
+};
