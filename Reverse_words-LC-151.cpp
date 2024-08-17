ca256d166dcf30c00cc905aa1eb7e58c254e7b09
Adit Garg <141175997+adit-garg@users.noreply.github.com>
2024-08-17 23:53:15 +0530

    Create Reverse_words-LC-151.cpp

---
+++ Reverse_words-LC-151.cpp
@@ -0,0 +1,25 @@
+// LeetCode Problem #151. Reverse Words in a String(0 ms)
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+        string s1 = "";
+        string c = "";
+        s = " " + s;
+        int l = 0;
+        for (int i = n; i >= 0; i--) {
+            l++;
+            if (s[i] == 32) {
+                c = s.substr(i, l);
+                if (c == " ") {
+                    l = 0;
+                } else {
+                    s1 += c;
+                }
+                l = 0;
+            }
+        }
+        s1.erase(0,1);
+        return s1;
+    }
+};
